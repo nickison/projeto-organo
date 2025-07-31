5c80580db153852203f26c737cda0b258bccfea0
michael nickison <michael_nickison@hotmail.com>
2025-07-31 19:37:47 -0300

    second commit

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,35 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = ()=>{
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão',
+    ]
+
+    const aoSalvar = (evento)=>{
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return(
-        <section>
-            <form>
-                <CampoTexto label='Nome'placeholder='Digite seu nome'/>
-                <CampoTexto label='Cargo'placeholder='Digite seu cargo'/>
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <CampoTexto obrigatorio={true}label='Nome'placeholder='Digite seu nome'/>
+                <CampoTexto obrigatorio={true} label='Cargo'placeholder='Digite seu cargo'/>
                 <CampoTexto label='Imagem'placeholder='Digite o endereço da imagem'/>
-                <CampoTexto label='time'placeholder=''/>
+                <ListaSuspensa obrigatorio={true} label='Time' itens={times}/>
+            <Botao>
+                Criar Card
+            </Botao>
             </form>
         </section>
     )
