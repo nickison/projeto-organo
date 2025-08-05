d8414bfa19cba8f05591913c62195d3fe292d8a0
michael nickison <michael_nickison@hotmail.com>
2025-08-04 21:28:07 -0300

    third commit

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,20 @@
 import './CampoTexto.css'
+import { useState } from 'react'
 
 const CampoTexto = (props)=>{
+
+const placeholderModificada = `${props.placeholder}...` 
+
+    const aoDigitado = (evento) =>{
+        props.aoAlterado(evento.target.value)
+    }
+
     return(
         <div className='campo-texto'>
-            <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <label>
+                {props.label}
+            </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada} />
         </div>
     )
 }
